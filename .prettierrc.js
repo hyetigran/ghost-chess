f7f014bc6cb460b905b364119356cba4493efb91
hyetigran <tigran.dev@protonmail.com>
2025-04-04 00:05:38 -0500

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  printWidth: 80,
+  endOfLine: 'auto',
+};
+
+module.exports = config;
