a415176ac47f7ec5bd2d7517737fc20957228cfa
hyetigran <tigran.dev@protonmail.com>
2025-04-06 10:46:47 -0500

    chore: add env and update config

---
+++ app.config.ts
@@ -0,0 +1,68 @@
+/* eslint-disable max-lines-per-function */
+import type { ConfigContext, ExpoConfig } from '@expo/config';
+
+import { ClientEnv, Env } from './env';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: Env.NAME,
+  description: `${Env.NAME} Mobile App`,
+  owner: Env.EXPO_ACCOUNT_OWNER,
+  scheme: Env.SCHEME,
+  slug: 'ghost-chess',
+  version: Env.VERSION.toString(),
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: Env.BUNDLE_ID,
+    config: {
+      usesNonExemptEncryption: false, // Avoid the export compliance warning on the app store
+    },
+  },
+  experiments: {
+    typedRoutes: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#2E3C4B',
+    },
+    package: Env.PACKAGE,
+    jsEngine: 'hermes',
+  },
+  web: {
+    favicon: './assets/images/favicon.png',
+    bundler: 'metro',
+  },
+  plugins: [
+    [
+      'expo-splash-screen',
+      {
+        backgroundColor: '#2E3C4B',
+        image: './assets/images/splash-icon.png',
+        imageWidth: 150,
+      },
+    ],
+    [
+      'expo-font',
+      {
+        fonts: ['./assets/fonts/Inter.ttf'],
+      },
+    ],
+    'expo-router',
+    ['react-native-edge-to-edge'],
+  ],
+  extra: {
+    ...ClientEnv,
+    eas: {
+      projectId: Env.EAS_PROJECT_ID,
+    },
+  },
+});
