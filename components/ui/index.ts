2cd3379d0b7a51e6f18f05d65f2d8b8abd263d56
hyetigran <tigran.dev@protonmail.com>
2025-04-06 18:39:48 -0500

    feat: add ui components

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -3,6 +3,7 @@ export * from './button';
 export * from './card';
 export * from './dialog';
 export * from './input';
+export * from './label';
 export * from './progress';
 export * from './select';
 export * from './switch';
